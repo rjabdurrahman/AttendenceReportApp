96fb0ec66bb55359da050e4d536ad8f5bb5c6336
Abdur Rahman <md.rahman0296@gmail.com>
2017-11-06 20:17:02 +0600

    String Filtered.

--- src/pdftest/JavaPDFTest.java
+++ src/pdftest/JavaPDFTest.java
@@ -5,10 +5,18 @@ import java.io.IOException;
 public class JavaPDFTest {
 
     public static void main(String[] args) throws IOException {
-
+        String[] date = new String[40];
        PDFManager pdfManager = new PDFManager();
        pdfManager.setFilePath("E:\\test.pdf");
-       System.out.println(pdfManager.ToText());       
+       //System.out.println(pdfManager.ToText().substring(376,704));
+       String alldate;
+       alldate = pdfManager.ToText().substring(376,704);
+       date[0] = alldate.substring(0,10);
+       date[1] = alldate.substring(11,21);
+       date[2] = alldate.substring(22,33);
+        System.out.println(date[0].length());
+       
+             
     
 }    
 }
